a287574df7b71a49012af555ff50217e726af3dc
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2023-12-01 19:28:43 +0300

    Serilog

---
+++ Core.CrossCuttingConcerns/Logging/LogDetailWithException.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogDetailWithException : LogDetail
+{
+    public string ExceptionMessage { get; set; }
+
+
+    public LogDetailWithException()
+    {
+        ExceptionMessage = string.Empty;
+    }
+
+    public LogDetailWithException(string fullName, string methodName, string user, List<LogParameters> parameters,
+        string exceptionMessage) : base(fullName, methodName, user, parameters)
+    {
+        ExceptionMessage = exceptionMessage;
+    }
+}
---
+++ Core.CrossCuttingConcerns/Logging/LogParameters.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogParameters
+{
+    public string Name { get; set; }
+    public object Value { get; set; }
+    public string Type { get; set; }
+
+    public LogParameters()
+    {
+        Name = string.Empty;
+        Value = string.Empty;
+        Type = string.Empty;
+    }
+
+    public LogParameters(string name, object value, string type)
+    {
+        Name = name;
+        Value = value;
+        Type = type;
+    }
+}
