943d862699314fc34c91e555ba27b470e55ef87f
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2023-12-01 03:58:02 +0300

    Cache Group Implementation

---
+++ Core.Application/Pipelines/Caching/CacheRemovingBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Text.Json;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CacheRemovingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheRemoverRequest
+{
+    private readonly IDistributedCache _distributedCache;
+
+    public CacheRemovingBehavior(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (request.BypassCache)
+        {
+            return await next();
+        }
+
+        TResponse response = await next();
+        
+        if(request.CacheGroupKey != null)
+        {
+            byte[]? cachedGroup = await _distributedCache.GetAsync(request.CacheGroupKey, cancellationToken);
+            if(cachedGroup != null)
+            {
+                HashSet<string> keysInGroup = JsonSerializer.Deserialize<HashSet<string>>(Encoding.Default.GetString(cachedGroup))!;
+                foreach (var key in keysInGroup)
+                {
+                    await _distributedCache.RemoveAsync(key, cancellationToken);
+                }
+                await _distributedCache.RemoveAsync(request.CacheGroupKey, cancellationToken);
+                await _distributedCache.RemoveAsync(key: $"{request.CacheGroupKey}SlidingExpiration", cancellationToken);
+            }
+        }        
+
+        if (request.CacheKey != null)
+        {
+            await _distributedCache.RemoveAsync(request.CacheKey, cancellationToken);
+        }
+
+        return response;
+    }
+}
