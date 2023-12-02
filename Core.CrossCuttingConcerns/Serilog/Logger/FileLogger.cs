c7532a7f6282bb6f8779d159be2c58651ceef7e7
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2023-12-02 03:37:15 +0300

    FileLogger (Serilog)

--- Core.CrossCuttingConcerns/Serilog/Logger/FileLogger.cs
+++ Core.CrossCuttingConcerns/Serilog/Logger/FileLogger.cs
@@ -14,7 +14,7 @@ public class FileLogger : LoggerServiceBase
         _configuration = configuration;
 
         FileLogConfiguration logConfig =
-            configuration.GetSection("seriLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
+            configuration.GetSection("SerilogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
             ?? throw new Exception(SerilogMessages.NullOptionsMessage);
 
         string logFilePath = string.Format(format: "{0}{1}",
