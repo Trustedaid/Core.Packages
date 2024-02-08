eae41522e53265b89be65903872ffc4bb3d788a5
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2024-02-08 17:17:09 +0300

    OtpNET configurations

---
+++ Core.Security/OTPAuthenticator/IOtpAuthenticatorHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Security.OTPAuthenticator;
+
+public interface IOtpAuthenticatorHelper
+{
+    public Task<byte[]> GenerateSecretKey();
+    public Task<string> ConvertSecretKeyToString(byte[] secretKey);
+    public Task<bool> VerifyCode(byte[] secretKey, string code);
+    
+}
