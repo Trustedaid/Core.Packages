56fbb72c01bd832a8e7a3f5aa2cf77fa1eba4844
Eren OĞUZ <92570103+Trustedaid@users.noreply.github.com>
2024-02-12 18:02:58 +0300

    Authorization Behavior

---
+++ Core.Security/SecurityServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Core.Security.JWT;
+using Core.Security.OTPAuthenticator;
+using Core.Security.OTPAuthenticator.OtpNet;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Security;
+
+public static class SecurityServiceRegistration
+{
+    public static IServiceCollection AddSecurityService(this IServiceCollection services)
+    {
+        services.AddScoped<ITokenHelper, JwtHelper>();
+        //services.AddScoped<IEmailAuthenticatorHelper, EmailAuthenticatorHelper>();
+        services.AddScoped<IOtpAuthenticatorHelper, OtpNetOtpAuthenticatorHelper>();
+
+        return services;
+    }
+}
